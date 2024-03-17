dafc31f48dcfbbccc1eab93ec4729dac267ba603
Siddhesh-Mali <129844841+Siddhesh-Mali@users.noreply.github.com>
2024-03-18 01:00:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
-}
+    // Write your code here
+    let character ;
+    for(let chara of str){
+        let count = 0;
+        for(let i=0; i<str.length;i++){
+            if(str.charAt(i)==chara){
+                count++;
+            }
+        }
+        if(count==1){
+           character = chara;
+        }
+    }
+    return character;
+   }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
